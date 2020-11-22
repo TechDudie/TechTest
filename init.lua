c4f8a4ceaa4db280f9c78487e69c9bb94827c63c
TechDudie <73961295+TechDudie@users.noreply.github.com>
2020-11-22 09:16:22 -0600

    Add Carrot Snacks

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
+minetest.register_craftitem("techtest:carrot_fruit_snacks", {
+        description = "Carrot Fruit Snacks",
+        inventory_image = "carrot.png",
+        on_use = minetest.item_eat(6),
+})
+minetest.register_craft({
+	output = "techtest:carrot_fruit_snacks",
+	recipe = {
+		{"bonemeal:gelatain_powder", "bucket:bucket_water"},
+		{"farming:carrot", ""}
+	},
+	replacements={{'bucket:bucket_water','bucket:bucket_empty'}}
+})
 minetest.register_craftitem("techtest:blueberry_fruit_snacks", {
         description = "Blueberry Fruit Snacks",
         inventory_image = "blueberry.png",
