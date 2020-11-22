74cc29ab29c5aa2510d4b65e3441961dbd3fd40a
TechDudie <73961295+TechDudie@users.noreply.github.com>
2020-11-21 22:25:51 -0600

    Add raspberry fruit snacks

--- init.lua
+++ init.lua
@@ -1,7 +1,24 @@
+minetest.register_craftitem("techtest:blueberry_fruit_snacks", {
+        description = "Blueberry Fruit Snacks",
+        inventory_image = "blueberry.png",
+        on_use = minetest.item_eat(4),
+})
 minetest.register_craft({
 	output = "techtest:blueberry_fruit_snacks",
 	recipe = {
 		{"bonemeal:gelatain_powder", "bucket:bucket_water"},
 		{"farming:blueberry", ""}
+	},
+})
+minetest.register_craftitem("techtest:raspberry_fruit_snacks", {
+        description = "Raspberry Fruit Snacks",
+        inventory_image = "Raspberry.png",
+        on_use = minetest.item_eat(4),
+})
+minetest.register_craft({
+	output = "techtest:raspberry_fruit_snacks",
+	recipe = {
+		{"bonemeal:gelatain_powder", "bucket:bucket_water"},
+		{"farming:raspberry", ""}
 	},
 })
